64d7985702d909dcda18b6b962b3b06d1a220cf0
Samuel Elgozi <samu.elgozi@gmail.com>
2018-03-07 22:40:45 +0200

    refactor: Restaurant's style and layout
    Single restaurants page is now responsive, and the css is cleaner and without many bugs and bad practices.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -28,7 +28,9 @@ fetchRestaurantFromURL = (callback) => {
     callback(null, self.restaurant)
     return;
   }
+
   const id = getParameterByName('id');
+
   if (!id) { // no id found in URL
     error = 'No restaurant id in URL'
     callback(error, null);
@@ -94,7 +96,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  * Create all reviews HTML and add them to the webpage.
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
-  const container = document.getElementById('reviews-container');
+  const container = document.querySelector('#reviews-container .width-limiter');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
@@ -117,19 +119,28 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
-  const name = document.createElement('p');
+  li.className = 'review';
+
+  const name = document.createElement('span');
+  name.className = 'username';
   name.innerHTML = review.name;
   li.appendChild(name);
 
-  const date = document.createElement('p');
+  const rating = document.createElement('span');
+  rating.className = 'rating';
+  // Add Star SVG for each rating point.
+  for(let i = 0; i < review.rating; i++) {
+    rating.innerHTML += '<svg width="14" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>';
+  }
+  li.appendChild(rating);
+
+  const date = document.createElement('span');
+  date.className = 'date';
   date.innerHTML = review.date;
   li.appendChild(date);
 
-  const rating = document.createElement('p');
-  rating.innerHTML = `Rating: ${review.rating}`;
-  li.appendChild(rating);
-
   const comments = document.createElement('p');
+  comments.className = 'body';
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
